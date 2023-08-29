98df58594133c4df65b51401c7f808aa78bfaa38
nomomax <nomomax@gmail.com>
2023-08-29 16:29:14 +0800

    migrate the shoppingCart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-final GlobalKey<ShoppingCartIconState> shoppingCart =
-    GlobalKey<ShoppingCartIconState>();
 final GlobalKey<ProductListWidgetState> productList =
     GlobalKey<ProductListWidgetState>();
 
@@ -175,7 +173,7 @@ class MyStorePageState extends State<MyStorePage> {
                   onPressed: _toggleSearch,
                   icon: const Icon(Icons.search, color: Colors.black),
                 ),
-              ShoppingCartIcon(key: shoppingCart),
+              const ShoppingCartIcon(),
             ],
             backgroundColor: Colors.white,
             pinned: true,
@@ -189,25 +187,12 @@ class MyStorePageState extends State<MyStorePage> {
   }
 }
 
-class ShoppingCartIcon extends StatefulWidget {
+class ShoppingCartIcon extends StatelessWidget {
   const ShoppingCartIcon({Key? key}) : super(key: key);
 
   @override
-  ShoppingCartIconState createState() => ShoppingCartIconState();
-}
-
-class ShoppingCartIconState extends State<ShoppingCartIcon> {
-  Set<String> get itemsInCart => _itemsInCart;
-  Set<String> _itemsInCart = <String>{};
-
-  set itemsInCart(Set<String> value) {
-    setState(() {
-      _itemsInCart = value;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final Set<String> itemsInCart = AppStateScope.of(context).itemsInCart;
     final bool hasPurchase = itemsInCart.isNotEmpty;
     return Stack(
       alignment: Alignment.center,
@@ -268,12 +253,10 @@ class ProductListWidgetState extends State<ProductListWidget> {
 
   void _handleAddToCart(String id) {
     itemsInCart = _itemsInCart..add(id);
-    shoppingCart.currentState!.itemsInCart = itemsInCart;
   }
 
   void _handleRemoveFromCart(String id) {
     itemsInCart = _itemsInCart..remove(id);
-    shoppingCart.currentState!.itemsInCart = itemsInCart;
   }
 
   Widget _buildProductTile(String id) {
